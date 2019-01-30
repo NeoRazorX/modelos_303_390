e56f6dd4b522fb79ce0c5dd487127e2458c4eadc
NeoRazorX <neorazorx@gmail.com>
2019-01-30 11:31:15 +0100

    Pequeña actualización de mantenimiento.

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # modelo_347
 Plugin que añade los modelos 303 y 390 de la hacienda española a FacturaScripts.
 
-https://www.facturascripts.com
+https://www.facturascripts.com/plugin/modelos_303_390
--- controller/modelo_390.php
+++ controller/modelo_390.php
@@ -1,8 +1,7 @@
 <?php
-
-/*
+/**
  * This file is part of FacturaSctipts
- * Copyright (C) 2016-2017  Carlos Garcia Gomez  neorazorx@gmail.com
+ * Copyright (C) 2016-2019 Carlos Garcia Gomez <neorazorx@gmail.com>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as
@@ -11,100 +10,101 @@
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU Affero General Public License for more details.
  * 
  * You should have received a copy of the GNU Affero General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
-
-require_model('ejercicio.php');
 
 /**
  * Description of modelo_390
  *
- * @author carlos
+ * @author Carlos Garcia Gomez <neorazorx@gmail.com>
  */
 class modelo_390 extends fs_controller
 {
-   public $ejercicio;
-   public $sejercicio;
-   
-   public function __construct()
-   {
-      parent::__construct(__CLASS__, 'Modelo 390', 'informes');
-   }
-   
-   protected function private_core()
-   {
-      $this->ejercicio = new ejercicio();
-      
-      $this->sejercicio = $this->ejercicio->get($this->empresa->codejercicio);
-      if( isset($_REQUEST['codejercicio']) )
-      {
-         $this->sejercicio = $this->ejercicio->get($_REQUEST['codejercicio']);
-      }
-   }
-   
-   public function desglosar_iva_compras()
-   {
-      $desglose_iva_compras = array();
-      
-      if( $this->db->table_exists('lineasivafactprov') )
-      {
-         $sql = "select iva,recargo,sum(neto) as neto,sum(totaliva) as totaliva,sum(totalrecargo) as totalrecargo"
-                    . " from lineasivafactprov where idfactura in (select idfactura from facturasprov"
-                    . " where fecha >= ".$this->empresa->var2str($this->sejercicio->fechainicio)
-                    . " and fecha <= ".$this->empresa->var2str($this->sejercicio->fechafin).")"
-                    . " group by iva,recargo order by iva asc, recargo asc;";
-         
-         $data = $this->db->select($sql);
-         if($data)
-         {
-            foreach($data as $d)
-            {
-               $desglose_iva_compras[] = array(
-                   'iva' => floatval($d['iva']),
-                   'recargo' => floatval($d['recargo']),
-                   'neto' => floatval($d['neto']),
-                   'totaliva' => floatval($d['totaliva']),
-                   'totalrecargo' => floatval($d['totalrecargo']),
-               );
+
+    /**
+     *
+     * @var ejercicio
+     */
+    public $ejercicio;
+
+    /**
+     *
+     * @var ejercicio
+     */
+    public $sejercicio;
+
+    public function __construct()
+    {
+        parent::__construct(__CLASS__, 'Modelo 390', 'informes');
+    }
+
+    protected function private_core()
+    {
+        $this->ejercicio = new ejercicio();
+
+        $this->sejercicio = $this->ejercicio->get($this->empresa->codejercicio);
+        if (isset($_REQUEST['codejercicio'])) {
+            $this->sejercicio = $this->ejercicio->get($_REQUEST['codejercicio']);
+        }
+    }
+
+    public function desglosar_iva_compras()
+    {
+        $desglose_iva_compras = array();
+
+        if ($this->db->table_exists('lineasivafactprov')) {
+            $sql = "select iva,recargo,sum(neto) as neto,sum(totaliva) as totaliva,sum(totalrecargo) as totalrecargo"
+                . " from lineasivafactprov where idfactura in (select idfactura from facturasprov"
+                . " where fecha >= " . $this->empresa->var2str($this->sejercicio->fechainicio)
+                . " and fecha <= " . $this->empresa->var2str($this->sejercicio->fechafin) . ")"
+                . " group by iva,recargo order by iva asc, recargo asc;";
+
+            $data = $this->db->select($sql);
+            if ($data) {
+                foreach ($data as $d) {
+                    $desglose_iva_compras[] = array(
+                        'iva' => floatval($d['iva']),
+                        'recargo' => floatval($d['recargo']),
+                        'neto' => floatval($d['neto']),
+                        'totaliva' => floatval($d['totaliva']),
+                        'totalrecargo' => floatval($d['totalrecargo']),
+                    );
+                }
             }
-         }
-      }
-      
-      return $desglose_iva_compras;
-   }
-   
-   public function desglosar_iva_ventas()
-   {
-      $desglose_iva_ventas = array();
-      
-      if( $this->db->table_exists('lineasivafactcli') )
-      {
-         $sql = "select iva,recargo,sum(neto) as neto,sum(totaliva) as totaliva,sum(totalrecargo) as totalrecargo"
-                    . " from lineasivafactcli where idfactura in (select idfactura from facturascli"
-                    . " where fecha >= ".$this->empresa->var2str($this->sejercicio->fechainicio)
-                    . " and fecha <= ".$this->empresa->var2str($this->sejercicio->fechafin).")"
-                    . " group by iva,recargo order by iva asc, recargo asc;";
-         
-         $data = $this->db->select($sql);
-         if($data)
-         {
-            foreach($data as $d)
-            {
-               $desglose_iva_ventas[] = array(
-                   'iva' => floatval($d['iva']),
-                   'recargo' => floatval($d['recargo']),
-                   'neto' => floatval($d['neto']),
-                   'totaliva' => floatval($d['totaliva']),
-                   'totalrecargo' => floatval($d['totalrecargo']),
-               );
+        }
+
+        return $desglose_iva_compras;
+    }
+
+    public function desglosar_iva_ventas()
+    {
+        $desglose_iva_ventas = array();
+
+        if ($this->db->table_exists('lineasivafactcli')) {
+            $sql = "select iva,recargo,sum(neto) as neto,sum(totaliva) as totaliva,sum(totalrecargo) as totalrecargo"
+                . " from lineasivafactcli where idfactura in (select idfactura from facturascli"
+                . " where fecha >= " . $this->empresa->var2str($this->sejercicio->fechainicio)
+                . " and fecha <= " . $this->empresa->var2str($this->sejercicio->fechafin) . ")"
+                . " group by iva,recargo order by iva asc, recargo asc;";
+
+            $data = $this->db->select($sql);
+            if ($data) {
+                foreach ($data as $d) {
+                    $desglose_iva_ventas[] = array(
+                        'iva' => floatval($d['iva']),
+                        'recargo' => floatval($d['recargo']),
+                        'neto' => floatval($d['neto']),
+                        'totaliva' => floatval($d['totaliva']),
+                        'totalrecargo' => floatval($d['totalrecargo']),
+                    );
+                }
             }
-         }
-      }
-      
-      return $desglose_iva_ventas;
-   }
+        }
+
+        return $desglose_iva_ventas;
+    }
 }
